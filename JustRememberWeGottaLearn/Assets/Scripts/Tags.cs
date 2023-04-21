21e478aa8a222dc3145bb5bd48841194ae688728
Nicholas Owen Weber <noweber@gmail.com>
2023-04-20 20:25:00 -0700

    Synced Tags with Editor and Code

--- JustRememberWeGottaLearn/Assets/Scripts/Tags.cs
+++ JustRememberWeGottaLearn/Assets/Scripts/Tags.cs
@@ -6,5 +6,6 @@ namespace Assets.Scripts
         public static string Player { get; private set; } = "Player";
         public static string Enemy { get; private set; } = "Enemy";
         public static string Collectible { get; private set; } = "Collectible";
+        public static string KunFuBeat { get; private set; } = "KunFuBeat";
     }
 }
