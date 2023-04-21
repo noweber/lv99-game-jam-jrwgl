9c0042ced846c0f20d2e7b18579525cac948187a
wo22neQ <jinqizha@usc.edu>
2023-04-21 13:26:18 -0700

    ability and upgrade

--- JustRememberWeGottaLearn/Assets/Scripts/Abilities/BruceLeeKick.cs
+++ JustRememberWeGottaLearn/Assets/Scripts/Abilities/BruceLeeKick.cs
@@ -17,6 +17,12 @@ public class BruceLeeKick : KungFu
     private Vector3 m_dashOrigin;
     private Vector3 m_dashTarget;
 
+    [SerializeField]
+    private bool isUpgrade;
+    private bool isUpgradeAttacking;
+
+    [SerializeField]
+    private float upgradeAttackMoveTime = 0;
 
     private void Awake()
     {
@@ -26,35 +32,133 @@ public class BruceLeeKick : KungFu
     public override void Attack()
     {
         //throw new System.NotImplementedException();
-        Vector3 offsetPosition = transform.position;
-        Vector3 kickDirection = Vector3.zero;
-        switch (Player.Instance.gameObject.GetComponent<TopDownPlayerController>()._currFaceDir)
+        if(!isUpgrade)
+            AttackOrigin();
+        else if(isUpgrade && !isUpgradeAttacking)
         {
-            case PlayerFaceDirection.right:
-                offsetPosition = transform.position + new Vector3(1 * forwardOffset, 0, 0);
-                kickDirection = Vector3.right;
-                break;
-            case PlayerFaceDirection.left:
-                offsetPosition = transform.position + new Vector3(-1 * forwardOffset, 0, 0);
-                kickDirection = Vector3.left;
-                break;
-            case PlayerFaceDirection.up:
-                offsetPosition = transform.position + new Vector3(0, 1* forwardOffset, 0);
-                kickDirection = Vector3.up;
-                break;
-            case PlayerFaceDirection.down:
-                offsetPosition = transform.position + new Vector3(0, -1 * forwardOffset, 0);
-                kickDirection = Vector3.down;
-                break;
-
+            AttackUpgrade();
         }
+    }
 
+    private void AttackUpgrade()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 20f, LayerMask.GetMask("Enemy"));
+        if (colliders.Length > 0)
+        {
+            Transform minTransform = colliders[0].transform;
+            float distance = (transform.position - minTransform.position).magnitude;
+            foreach (var c in colliders)
+            {
+                float tmp = (transform.position - c.transform.position).magnitude;
+                if (tmp < distance)
+                {
+                    minTransform = c.transform;
+                    distance = tmp;
+                }
+            }
 
+            Vector3 dir2Enemy = minTransform.position - transform.position;
 
-        Instantiate(attackAbility, offsetPosition, transform.rotation);
-        PushBox pushbox = Instantiate(pushBox, offsetPosition, transform.rotation);
-        pushbox.SetDir(kickDirection);
+            float angle = Mathf.Atan2(dir2Enemy.y, dir2Enemy.x) * Mathf.Rad2Deg;
+            if(dir2Enemy.magnitude >= 8)
+            {
+                isUpgradeAttacking = true;
+                m_dashOrigin = transform.position;
+                m_dashTarget = transform.position + dir2Enemy.normalized * (dir2Enemy.magnitude - 8);
+                upgradeAttackMoveTime = DashTime;
+            }
+            else
+            {
+                isUpgradeAttacking = false;
+                Instantiate(attackAbility, transform.position + dir2Enemy.normalized * 4.0f, Quaternion.Euler(new Vector3(0, 0, angle)));
+                PushBox pushbox = Instantiate(pushBox, transform.position + dir2Enemy.normalized * 4.0f, Quaternion.Euler(new Vector3(0, 0, angle)));
+                pushbox.SetDir(dir2Enemy);
+            }
+        }
+        else
+        {
+            isUpgradeAttacking = false;
+            Vector3 offsetPosition = transform.position;
+            Vector3 kickDirection = Vector3.zero;
+            switch (Player.Instance.gameObject.GetComponent<TopDownPlayerController>()._currFaceDir)
+            {
+                case PlayerFaceDirection.right:
+                    offsetPosition = transform.position + new Vector3(1 * forwardOffset, 0, 0);
+                    kickDirection = Vector3.right;
+                    break;
+                case PlayerFaceDirection.left:
+                    offsetPosition = transform.position + new Vector3(-1 * forwardOffset, 0, 0);
+                    kickDirection = Vector3.left;
+                    break;
+                case PlayerFaceDirection.up:
+                    offsetPosition = transform.position + new Vector3(0, 1 * forwardOffset, 0);
+                    kickDirection = Vector3.up;
+                    break;
+                case PlayerFaceDirection.down:
+                    offsetPosition = transform.position + new Vector3(0, -1 * forwardOffset, 0);
+                    kickDirection = Vector3.down;
+                    break;
 
+            }
+
+            Instantiate(attackAbility, offsetPosition, transform.rotation);
+            PushBox pushbox = Instantiate(pushBox, offsetPosition, transform.rotation);
+            pushbox.SetDir(kickDirection);
+        }
+    }
+
+    private void AttackOrigin()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 8.0f, LayerMask.GetMask("Enemy"));
+        if (colliders.Length > 0)
+        {
+            Transform minTransform = colliders[0].transform;
+            float distance = (transform.position - minTransform.position).magnitude;
+            foreach (var c in colliders)
+            {
+                float tmp = (transform.position - c.transform.position).magnitude;
+                if (tmp < distance)
+                {
+                    minTransform = c.transform;
+                    distance = tmp;
+                }
+            }
+
+            Vector3 dir2Enemy = (minTransform.position - transform.position).normalized;
+            float angle = Mathf.Atan2(dir2Enemy.y, dir2Enemy.x) * Mathf.Rad2Deg;
+            Instantiate(attackAbility, transform.position + dir2Enemy * 4.0f, Quaternion.Euler(new Vector3(0, 0, angle)));
+            PushBox pushbox = Instantiate(pushBox, transform.position + dir2Enemy * 4.0f, Quaternion.Euler(new Vector3(0, 0, angle)));
+            pushbox.SetDir(dir2Enemy);
+        }
+        else
+        {
+            Vector3 offsetPosition = transform.position;
+            Vector3 kickDirection = Vector3.zero;
+            switch (Player.Instance.gameObject.GetComponent<TopDownPlayerController>()._currFaceDir)
+            {
+                case PlayerFaceDirection.right:
+                    offsetPosition = transform.position + new Vector3(1 * forwardOffset, 0, 0);
+                    kickDirection = Vector3.right;
+                    break;
+                case PlayerFaceDirection.left:
+                    offsetPosition = transform.position + new Vector3(-1 * forwardOffset, 0, 0);
+                    kickDirection = Vector3.left;
+                    break;
+                case PlayerFaceDirection.up:
+                    offsetPosition = transform.position + new Vector3(0, 1 * forwardOffset, 0);
+                    kickDirection = Vector3.up;
+                    break;
+                case PlayerFaceDirection.down:
+                    offsetPosition = transform.position + new Vector3(0, -1 * forwardOffset, 0);
+                    kickDirection = Vector3.down;
+                    break;
+
+            }
+
+            Instantiate(attackAbility, offsetPosition, transform.rotation);
+            PushBox pushbox = Instantiate(pushBox, offsetPosition, transform.rotation);
+            pushbox.SetDir(kickDirection);
+        }
     }
 
     protected override void Dash()
@@ -88,7 +192,7 @@ public class BruceLeeKick : KungFu
     private void FixedUpdate()
     {
       
-        if (m_remainDashTime > 0)
+        if (m_remainDashTime > 0 && !isUpgradeAttacking)
         {
            
             Vector3 nextPosition = Vector3.Lerp(m_dashOrigin, m_dashTarget, (1 - m_remainDashTime / DashTime));
@@ -98,6 +202,26 @@ public class BruceLeeKick : KungFu
             PlayerAfterImagePool.Instance.GetFromPool();
         }
         
+        if(isUpgradeAttacking)
+        {
+            Vector3 nextPosition = Vector3.Lerp(m_dashOrigin, m_dashTarget, (1 - upgradeAttackMoveTime / DashTime));
+            upgradeAttackMoveTime -= Time.deltaTime;
+            Player.Instance.playerController.MoveToPosition(nextPosition);
+            PlayerAfterImagePool.Instance.GetFromPool();
+            if(upgradeAttackMoveTime <= 0)
+            {
+                Debug.Log("awd");
+                Vector3 dir2Enemy = (m_dashTarget - m_dashOrigin).normalized;
+                float angle = Mathf.Atan2(dir2Enemy.y, dir2Enemy.x) * Mathf.Rad2Deg;
+                Instantiate(attackAbility, transform.position + dir2Enemy * 4.0f, Quaternion.Euler(new Vector3(0, 0, angle)));
+                PushBox pushbox = Instantiate(pushBox, transform.position + dir2Enemy * 4.0f, Quaternion.Euler(new Vector3(0, 0, angle)));
+                pushbox.SetDir(dir2Enemy);
+                isUpgradeAttacking = false;
+                upgradeAttackMoveTime = 0;
+            }
+        }
+
+
     }
     
 }
--- JustRememberWeGottaLearn/Assets/Scripts/Abilities/Shaolin.cs
+++ JustRememberWeGottaLearn/Assets/Scripts/Abilities/Shaolin.cs
@@ -12,16 +12,63 @@ public class Shaolin : KungFu
     private Vector3 m_dashTarget;
 
     public GameObject attackAbility;
+
+    [SerializeField]
+    private bool isUpgrade;
+
     protected void Awake()
     {
         _stance = Stance.stance.ShaolinKungFu;
     }
-    
+
     public override void Attack()
     {
         //throw new System.NotImplementedException();
-        Instantiate(attackAbility, transform.position, transform.rotation);
+        //Instantiate(attackAbility, transform.position, transform.rotation);
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 6.0f, LayerMask.GetMask("Enemy"));
+        if (colliders.Length > 0)
+        {
+            Transform minTransform = colliders[0].transform;
+            float distance = (transform.position - minTransform.position).magnitude;
+            foreach (var c in colliders)
+            {
+                float tmp = (transform.position - c.transform.position).magnitude;
+                if (tmp < distance)
+                {
+                    minTransform = c.transform;
+                    distance = tmp;
+                }
+            }
+
+            Vector3 dir2Enemy = (minTransform.position - transform.position).normalized;
+            foreach (var c in colliders)
+            {
+                Vector2 dir = (c.transform.position - transform.position).normalized;
+                if (!isUpgrade)
+                {
+                    float angleBetween = Vector2.Angle(dir, dir2Enemy);
+                    if (angleBetween <= 60 / 2f)
+                    {
+                        Instantiate(attackAbility, c.transform.position, c.transform.rotation);
+                    }
+                }
+                else
+                {
+                    float angleBetweenInverse = Vector2.Angle(dir, -dir2Enemy);
+                    float angleBetween = Vector2.Angle(dir, dir2Enemy);
+                    Debug.Log(angleBetweenInverse);
+                    if (angleBetween <= 60 / 2f || angleBetweenInverse <= 60 / 2f)
+                    {
+                        Instantiate(attackAbility, c.transform.position, c.transform.rotation);
+                    }
+                }
+
+            }
+        }
+
     }
+
+
 
     protected override void Dash()
     {
